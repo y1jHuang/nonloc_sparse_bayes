1c8baeed7032e104285871dc7c76ec91bfb59cfc
y1jHuang <yingjie_huang@brown.edu>
2024-09-28 00:53:57 +0800

    data_gen changes

--- scripts/data_gen.R
+++ scripts/data_gen.R
@@ -14,28 +14,28 @@ gen_eta <- function(Z){
   Z <- as.matrix(Z)
   num_slab <- sum(Z)
   eta <- matrix(0, nrow(Z), ncol(Z))
-  rand <- rnorm(num_slab)
-  # rand <- rbiunif(num_slab, 0.7, 1)
+  # rand <- rnorm(num_slab)
+  rand <- sapply(1:num_slab, function(x){MH(func=pmom, step_size=0.1, psi=0.5)})
   eta[Z==1] <- rand
   return(eta)
 }
-# rbiunif <- function(n=1, min=0.7, max=1){
-#   sign <- rbinom(n, 1, 0.5)
-#   (sign - 0.5) * 2 * runif(n, min, max)
-# }
+rbiunif <- function(n=1, min=0.7, max=1){
+  sign <- rbinom(n, 1, 0.5)
+  (sign - 0.5) * 2 * runif(n, min, max)
+}
 data <- lapply(1:num_rep, function(rep){
   # Simulation for Lambda, Sigma and epsilon
   Lambda <- sapply(1:k, function(i){
     lambda <- rep(0, p)
     idx <- sample(1:p, num_eff[i]) # index of non-zero elements in each column
-    # lambda[idx] <- rbiunif(num_eff[i], 0.7, 1)
-    lambda[idx] <- rnorm(num_eff[i], 0, 9) # assign values to non-zero elements
+    lambda[idx] <- rbiunif(num_eff[i], 0.7, 1)
+    # lambda[idx] <- rnorm(num_eff[i], 0, 9) # assign values to non-zero elements
     return(lambda)
   })
   Z <- matrix(rbinom(N*k, 1, p0), N, k)
   eta <- gen_eta(Z)
   # Sigma <- diag(1/rgamma(p, shape=1, rate=0.25))
-  Sigma <- diag(runif(p, 0, 1))
+  Sigma <- diag(runif(p, 0, 0.5))
   epsilon <- LaplacesDemon::rmvn(N, 0, Sigma)
   Y <- eta %*% t(Lambda) + epsilon
   return(list(Y=Y, Lambda=Lambda, eta=eta))
